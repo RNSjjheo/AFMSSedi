90b1f863d1bbb13a4bb390994af15805db8d583e
RNSjjheo <jjheo@rnsea.com>
2026-08-31 19:40:42 +0900

    a

--- AFMSSSCService/Source/Main/Program.cs
+++ AFMSSSCService/Source/Main/Program.cs
@@ -1,18 +1,27 @@
 using AFMSSediDll;
+using log4net;
 
 namespace AFMSSSCService
 {
     public class Program
     {
         private const string PROCESS_NAME = "AFMSSSCService";
+        private static readonly ILog Log = LogManager.GetLogger(PROCESS_NAME);
 
         public static void Main(string[] args)
         {
             AFMSLog.Initialize(Environment.UserInteractive, PROCESS_NAME);
-            AFMSLogBanner.WriteStartup(PROCESS_NAME, "AFMS DISCHARGE SERVICE");
+            AFMSLogBanner.WriteStartup(PROCESS_NAME, "AFMS SSC SERVICE");
+
+            string programPath = Environment.ProcessPath ?? throw new InvalidOperationException("현재 프로그램 경로를 확인할 수 없습니다.");
+            ServiceInstallResult installResult = WindowsServiceManager.EnsureInstalled(programPath, PROCESS_NAME);
+            Log.Info(installResult.Status);
+            Log.Info(installResult.Message);
+
+            FBProvider.Instance.Initialize(FBProvider.SetFBConnStrBuilder());
 
             var builder = Host.CreateApplicationBuilder(args);
-            builder.Services.AddHostedService<Worker>();
+            builder.Services.AddHostedService<WorkerSSC>();
 
             var host = builder.Build();
             host.Run();
